f390b725bee62f870315008d70df0c3503d97c90
raphael franco <raphaelmilanez@yahoo.com.br>
2018-08-31 16:58:35 -0300

    first commit

---
+++ src/ProdutosNovo.js
@@ -0,0 +1,45 @@
+import React, { Component} from 'react'
+import { Redirect } from 'react-router-dom'
+
+class Produtos extends Component{
+    constructor(props){
+        super(props)
+
+        this.handleNewProduto = this.handleNewProduto.bind(this)
+
+        this.state = {
+            redirect: false
+        }
+
+    }
+    handleNewProduto(){
+        const produto ={
+            produtos: this.refs.produto.value,
+            categoria: this.refs.categoria.value
+        }
+        this.props.createProduto(produto)
+          .then((res)=> this.setState({redirect: '/produtos/categoria/'+produto.categoria}))
+
+    }
+    render(){
+       const { categorias } = this.props
+       if(this.state.redirect){
+           return <Redirect to={this.state.redirect} />
+        }
+       return (<div>
+                <h2>Novo Produto</h2>
+                <select ref='categoria'>
+                   {categorias
+                        .map((c) => <option key={c.id} value={c.id}>{c.categoria}</option>)
+                    }
+                </select>
+                <input 
+                    placeholder='Nome do novo Produto'
+                    className='form-control'
+                    ref='produto' />
+                    <button onClick={this.handleNewProduto}>Salvar</button>
+                </div>)
+    }
+}
+
+export default Produtos
